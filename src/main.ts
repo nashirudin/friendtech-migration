1e449f04250f80fbff86825c9c5bc4bf1673474d
nashirudin <35358228+nashirudin@users.noreply.github.com>
2024-01-30 03:58:36 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,44 @@ import { ID } from '@subsquid/graphql-server';
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
 
-    let OwnershipTransferred: Map<string,OwnershipTransferred > = new Map();
-
-    for (let c of ctx.blocks) {
-        for (let log of c.logs) {
-            if (log.address !== CONTRACT_ADDRESS || log.topics[0] !== FriendtechSharesV1ABI.events.OwnershipTransferred.topic, FriendtechSharesV1ABI.events.Trade.topic) continue
-            const { previousOwner, newOwner } = FriendtechSharesV1ABI.events.OwnershipTransferred.decode(log)
-            const { trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply } = FriendtechSharesV1ABI.events.Trade.decode(log)
-            
+   const trades: Trade[] = [];
+    const ownershipTransfers: OwnershipTransferred[] = [];
+    for (let block of ctx.blocks) {
+        for (let log of block.logs) {
+          if (log.topics[0] === FriendtechSharesV1ABI.events.Trade.topic) {
+            let event = FriendtechSharesV1ABI.events.Trade.decode(log);
+            let trade = new Trade({
+              id: log.transaction?.hash,
+              trader: event.trader,
+              subject: event.subject,
+              isBuy: event.isBuy,
+              shareAmount: event.shareAmount,
+              ethAmount: event.ethAmount,
+              protocolEthAmount: event.protocolEthAmount,
+              subjectEthAmount: event.subjectEthAmount,
+              supply: event.supply,
+              blockNumber: BigInt(block.header.height),
+              blockTimestamp: BigInt(block.header.timestamp),
+              transactionHash: log.transaction?.hash,
+            });
+            trades.push(trade);
     
-
-ctx.store.upsert([...OwnershipTransferred.values()]);
-
+        }
+        if (log.topics[0] === FriendtechSharesV1ABI.events.OwnershipTransferred.topic) {
+            let event = FriendtechSharesV1ABI.events.OwnershipTransferred.decode(log);
+            let ownershipTransferred = new OwnershipTransferred({
+              id: log.transaction?.hash,
+              previousOwner: event.previousOwner,
+              newOwner: event.newOwner,
+              transactionHash: log.transaction?.hash,
+              blockNumber: BigInt(block.header.height),
+              blockTimestamp: BigInt(block.header.timestamp),
+            });
+            ownershipTransfers.push(ownershipTransferred);
+            
+    }
 }
 }
+await ctx.store.upsert([...trades.values()]);
+await ctx.store.upsert([...ownershipTransfers.values()]);
 })
